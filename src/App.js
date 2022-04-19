95370d163b3edd8d3db6493174a8c56634ba5101
dao vo <gary.vo32@gmail.com>
2022-04-19 14:14:36 -0700

    able to clear input field when task is added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React, { Component } from "react";
-import Task from "./components/Overview";
+import Overview from "./components/Overview";
+import uniqid from "uniqid";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      task: { text: "" },
+      task: {
+        text: "",
+        id: uniqid(),
+      },
       tasks: [],
     };
   }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {
+        task: "",
+        id: uniqid(),
+      },
+    });
+    document.querySelector("#taskInput").value = "";
+  };
 
   render() {
     const { task, tasks } = this.state;
 
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmitTask}>
           <label htmlFor="taskInput">Enter Task</label>
-          <input type="text" id="taskInput" />
+          <input
+            type="text"
+            id="taskInput"
+            value={task.text}
+            onChange={this.handleChange}
+          />
           <button type="submit">Add Task</button>
         </form>
+        <Overview tasks={tasks} />
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,38 +1,50 @@
 import React from "react";
 
-class Task extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { tasks: [] };
-  }
+const Overview = (props) => {
+  const { tasks } = props;
 
-  handleClick = () => {
-    const input = document.querySelector(".user-input");
-    console.log(input.value);
-    const updatedTask = [...this.state.tasks, input.value];
-    console.log(updatedTask);
-    this.setState({ tasks: updatedTask });
-    this.appendTask();
-  };
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return <li key={task.id}>{task.text}</li>;
+      })}
+    </ul>
+  );
+};
 
-  appendTask = () => {
-    for (let i = 0; i < this.state.tasks.length; i++) {
-      <li>{this.state.tasks[i]}</li>;
-    }
-  };
+// class Task extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { tasks: [] };
+//   }
 
-  render() {
-    return (
-      <section>
-        <input type="text" className="user-input"></input>
-        <button className="addTaskButt" onClick={this.handleClick}>
-          add task
-        </button>
-        <ul className="tasks">
-        </ul>
-      </section>
-    );
-  }
-}
+//   handleClick = () => {
+//     const input = document.querySelector(".user-input");
+//     console.log(input.value);
+//     const updatedTask = [...this.state.tasks, input.value];
+//     console.log(updatedTask);
+//     this.setState({ tasks: updatedTask });
+//     this.appendTask();
+//   };
 
-export default Task;
+//   appendTask = () => {
+//     for (let i = 0; i < this.state.tasks.length; i++) {
+//       <li>{this.state.tasks[i]}</li>;
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <section>
+//         <input type="text" className="user-input"></input>
+//         <button className="addTaskButt" onClick={this.handleClick}>
+//           add task
+//         </button>
+//         <ul className="tasks">
+//         </ul>
+//       </section>
+//     );
+//   }
+// }
+
+export default Overview;
