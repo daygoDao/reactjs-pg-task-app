32cce49367db3fc771720febd0c2f44fd00bc11a
dao vo <gary.vo32@gmail.com>
2022-04-22 14:59:50 -0700

    converting away from classes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,18 @@
 import React, { Component } from "react";
-import Overview from "./components/Overview";
+import Header from "./components/Header";
 import uniqid from "uniqid";
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      task: {
-        text: "",
-        id: uniqid(),
-      },
-      tasks: [],
-    };
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      task: {
-        text: e.target.value,
-        id: this.state.task.id,
-      },
-    });
-  };
-
-  onSubmitTask = (e) => {
-    e.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.concat(this.state.task),
-      task: {
-        task: "",
-        id: uniqid(),
-      },
-    });
-    document.querySelector("#taskInput").value = "";
-  };
-
-  render() {
-    const { task, tasks } = this.state;
-
-    return (
-      <div>
-        <form onSubmit={this.onSubmitTask}>
-          <label htmlFor="taskInput">Enter Task</label>
-          <input
-            type="text"
-            id="taskInput"
-            value={task.text}
-            onChange={this.handleChange}
-          />
-          <button type="submit">Add Task</button>
-        </form>
-        <Overview tasks={tasks} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <form>
+        <label htmlFor="taskInput">Enter Task</label>
+        <input type="text" id="taskInput" />
+        <button type="submit">Add Task</button>
+      </form>
+      <Header />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = (props) => {
+  const { tasks } = props;
+
+  return (
+    <div>
+      <h1>Task Tracker</h1>
+    </div>
+  );
+};
+
+export default Header;
